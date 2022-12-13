559ce53fb92dfc2a379c2fd6c4aedd91b9b19046
styled <smyvensestime@gmail.com>
2022-12-13 09:56:59 -0500

    ensure that the font's are loaded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 export default async function Page() {
     return (
         <div className="w-full h-screen flex items-center justify-center flex-col gap-3">
-            <h1 className="bold text-6xl text-primary font-gilroy font-bold">Hello World</h1>
-            <h3 className="bold text-3xl text-primary font-poppins">So happy to have you</h3>
+            <h1 className="text-6xl text-secondary-600 font-loudrina-shadow">Smyvens</h1>
+            <h2 className="text-3xl text-container-600 font-gilroy font-extralight">Hello World</h2>
+            <h2 className="text-3xl text-primary-600 font-poppins font-extralight">
+                So happy to have you
+            </h2>
         </div>
     )
 }
