f5a2c942e82a0aa85aa805aa99f0e29e97265d4c
styled <smyvensestime@gmail.com>
2022-12-22 13:30:23 -0500

    refractor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-"use client"
-
-import { useRef } from "react"
-import PopupWindow from "~/components/PopupWindow"
-
 export default function Page() {
-    const showPopup = useRef<HTMLButtonElement>(null)
     return (
         <div className="w-full h-screen flex items-center justify-center flex-col gap-3">
             <h1 className="text-6xl text-secondary-600 font-loudrina-shadow">Smyvens</h1>
@@ -12,16 +6,6 @@ export default function Page() {
             <h2 className="text-3xl text-primary-600 font-poppins font-extralight">
                 So happy to have you
             </h2>
-            <button type="button" ref={showPopup}>
-                show popup
-            </button>
-            <PopupWindow trigger={showPopup} location={{ left: "600px", top: "100px" }}>
-                <p className="w-96">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, quam dolor,
-                    asperiores, doloremque temporibus cupiditate laborum harum ipsum ipsa facilis
-                    fuga vitae necessitatibus sequi quas n
-                </p>
-            </PopupWindow>
         </div>
     )
 }
