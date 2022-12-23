ccde50427d9adbee12e59fe3d9ed52d4d314e3fe
smyvens <55722176+styled-man@users.noreply.github.com>
2022-12-22 22:35:03 -0500

    Feature/popup window refractor (#11)
    * made popup window and test more readable
    
    * added an optional callback function that runs whenever the popup is either opened or closed
    
    * add the ability to close the popup with multipe triggers
    
    * remove unused variable
    
    * fix only one test runs

--- src/components/PopupWindow/index.tsx
+++ src/components/PopupWindow/index.tsx
@@ -1,116 +1,146 @@
-"use client"
-
-import {
-    MouseEventHandler,
-    ReactNode,
-    RefObject,
-    useCallback,
-    useEffect,
-    useRef,
-    useState,
-} from "react"
 import { Property } from "csstype"
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react"
 
-type Cord = `${number}px`
+type Location = `${number}px`
+type Trigger = (HTMLElement | null) | (HTMLElement | null)[]
 
 interface PopupWindowProps {
     children: ReactNode
-    location: {
-        left: Cord
-        top: Cord
+    triggers: {
+        open: Trigger
+        close?: Trigger
     }
-    trigger: RefObject<Element | null>
+    location: {
+        top: Location
+        left: Location
+    }
+    onChange?: (isOpen: boolean) => void
+    // styles
     backgroundColor?: Property.BackgroundColor
     borderColor?: Property.BorderColor
     pageTint?: Property.BackgroundColor
 }
 
 export default function PopupWindow({
-    location,
     children,
-    trigger,
+    triggers,
+    location: givenLocation,
+    onChange,
     borderColor,
     backgroundColor = "white",
     pageTint = "#66000000",
 }: PopupWindowProps) {
     const [isOpen, setOpen] = useState(false)
-    const wrapperRef = useRef<HTMLDivElement>(null)
-    const [adjustedLocation, setAdjustedLocation] = useState(location)
+    const popupWrapperRef = useRef<HTMLDivElement>(null)
 
-    // ensure that the popup is always on the screen, even if the cords provided are off the screen
-    const calculateNewLocation = (given: Cord, current: number, max: number): Cord => {
+    const changePopupState = () => {
+        setOpen(val => {
+            return !val
+        })
+        if (onChange) {
+            onChange(!isOpen)
+        }
+    }
+
+    // add/remove click event listeners for triggers
+    const handleTriggers = (trigger: Trigger | undefined, onClick: (event: MouseEvent) => void) => {
+        if (!trigger) return undefined
+
+        // convert a single trigger to an array
+        const triggerArray = Array.isArray(trigger) ? trigger : [trigger]
+
+        // add one event listener for each trigger
+        triggerArray.forEach(trig => {
+            trig?.addEventListener("click", onClick)
+        })
+
+        return () => {
+            // whenever the popup is closed, remove the event listener from the screen
+            triggerArray.forEach(trig => {
+                trig?.removeEventListener("click", onClick)
+            })
+        }
+    }
+
+    // click event listeners that open the popup window
+    useEffect(() => {
+        return handleTriggers(triggers.open, changePopupState)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [triggers.open])
+
+    // click event listeners that open the popup window
+    useEffect(() => {
+        return handleTriggers(triggers.close, changePopupState)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [triggers.close])
+
+    // **************************************************
+
+    /**
+     *
+     * @param given the location that was pass to this component as part of the props
+     * @param popupDimension The current width or height of the popup. ps: popup dimension
+     * @param screenRestriction the total width or height of the screen. ps: document.documentElement.*
+     */
+    const ensurePopupIsAlwaysOnScreen = (
+        given: Location,
+        popupDimension: number,
+        screenRestriction: number
+    ): Location => {
         const padding = 10
-        // console.log(+given.replace("px", ""))
-        const givenLocation = +given.replace("px", "") < max * 2 ? +given.replace("px", "") : max
 
-        if (givenLocation + current > max) {
-            const overflow = (givenLocation + current) % max
-            const newLocation = givenLocation - overflow - padding
+        // convert location to number
+        const location =
+            +given.replace("px", "") < screenRestriction * 2
+                ? +given.replace("px", "")
+                : screenRestriction
+
+        // check if the popup is partially or totally outside the window
+        if (location + popupDimension > screenRestriction) {
+            const overflow = (location + popupDimension) % screenRestriction
+            const newLocation = location - overflow - padding
 
             // make sure that the popup is not off the screen on the opposite direction
             return `${newLocation > 0 ? newLocation : 0}px`
         }
 
-        return `${givenLocation}px`
+        return `${location}px`
     }
 
+    const [popupLocation, setPopupLocation] = useState(givenLocation)
     const popupRef = useCallback(
-        (node: HTMLDivElement | null) => {
-            if (!node) return
+        (popup: HTMLDivElement) => {
+            if (!popup) return
 
-            setAdjustedLocation({
-                left: calculateNewLocation(
-                    location.left,
-                    node.clientWidth,
+            setPopupLocation({
+                left: ensurePopupIsAlwaysOnScreen(
+                    givenLocation.left,
+                    popup.clientWidth,
                     document.documentElement.clientWidth
                 ),
-                top: calculateNewLocation(
-                    location.top,
-                    node.clientHeight,
+                top: ensurePopupIsAlwaysOnScreen(
+                    givenLocation.top,
+                    popup.clientHeight,
                     document.documentElement.clientHeight
                 ),
             })
         },
-        [location]
+        [givenLocation]
     )
-
-    const handleClose: MouseEventHandler<HTMLDivElement> = event => {
-        event.preventDefault()
-        event.stopPropagation()
-
-        if (event.target === wrapperRef.current) {
-            setOpen(false)
-        }
-    }
-
-    const handleOpen = (event: Event) => {
-        event.preventDefault()
-        event.stopPropagation()
-        setOpen(true)
-    }
-
-    useEffect(() => {
-        const el = trigger.current
-
-        el?.addEventListener("click", handleOpen)
-        return () => {
-            el?.removeEventListener("click", handleOpen)
-        }
-    }, [trigger])
 
     return isOpen ? (
         <div
-            className="fixed w-screen h-screen z-50"
-            data-test-id="popup-window-wrapper"
-            onClick={handleClose}
-            ref={wrapperRef}
+            className="fixed w-full h-screen z-50"
+            onClick={element => element.target === popupWrapperRef.current && changePopupState()}
             style={{ backgroundColor: pageTint }}
+            ref={popupWrapperRef}
+            data-test-id="popup-window-wrapper"
         >
             <div
-                style={{ ...adjustedLocation, backgroundColor, borderColor }}
-                data-test-id="popup-window"
-                ref={popupRef}
                 className="fixed p-1 rounded-md shadow-around border border-container-300"
+                style={{ ...popupLocation, backgroundColor, borderColor }}
+                ref={popupRef}
+                data-test-id="popup-window"
             >
                 {children}
             </div>
